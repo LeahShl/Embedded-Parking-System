f255ef23ac11f4dedc8e10a054e17c4189e03a4c
Leah <shlemov@tuta.io>
2025-07-29 12:01:32 +0300

    STM32: formatted timestamp print

--- STM32/GPS_sim/Src/i2c_send.c
+++ STM32/GPS_sim/Src/i2c_send.c
@@ -8,11 +8,14 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include <stdio.h>
+#include <time.h>
 
 #define I2C_TIMO 1000 // I2C timeout ms
 
 extern I2C_HandleTypeDef hi2c1;
 extern osMessageQueueId_t gpsMsgQueueHandle;
+
+static void utc_to_str(uint32_t utc_sec, char *buf);
 
 void StartI2CSenderTask(void *argument)
 {
@@ -26,8 +29,11 @@ void StartI2CSenderTask(void *argument)
 			else if(msg.msg_type == MSG_START) type_str = "START";
 			else if(msg.msg_type == MSG_STOP) type_str = "STOP";
 
-			printf("Sending %s message: license=%08lu, time(sec)=%lu, lat=%.6f, lon=%.6f\n",
-		           type_str, (unsigned long)msg.license_id, (unsigned long)msg.utc_seconds,
+			char buf[64];
+			utc_to_str(msg.utc_seconds, buf);
+
+			printf("Sending %s message: license=%08lu, %s, lat=%.6f, lon=%.6f\n",
+		           type_str, (unsigned long)msg.license_id, buf,
 		           msg.latitude, msg.longitude);
 
 			HAL_StatusTypeDef ret = HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t*)&msg, sizeof(msg), I2C_TIMO);
@@ -39,4 +45,14 @@ void StartI2CSenderTask(void *argument)
 		}
 
 	}
+}
+
+static void utc_to_str(uint32_t utc_sec, char *buf)
+{
+	time_t utc_tim = utc_sec;
+	struct tm *ts;
+
+	ts = gmtime(&utc_tim);
+
+	strftime(buf, 64, "%Y-%m-%d %H:%M:%S UTC", ts);
 }
