4bfa69e17d02cccfb78ae484d5d1cd1ee198ede2
Leah <shlemov@tuta.io>
2025-07-29 10:53:37 +0300

    added timeout to i2c sender for improved robustness

--- STM32/GPS_sim/Src/i2c_send.c
+++ STM32/GPS_sim/Src/i2c_send.c
@@ -9,6 +9,8 @@
 #include "cmsis_os.h"
 #include <stdio.h>
 
+#define I2C_TIMO 1000 // I2C timeout ms
+
 extern I2C_HandleTypeDef hi2c1;
 extern osMessageQueueId_t gpsMsgQueueHandle;
 
@@ -28,7 +30,7 @@ void StartI2CSenderTask(void *argument)
 		           type_str, (unsigned long)msg.license_id, (unsigned long)msg.utc_seconds,
 		           msg.latitude, msg.longitude);
 
-			HAL_StatusTypeDef ret = HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t*)&msg, sizeof(msg), HAL_MAX_DELAY);
+			HAL_StatusTypeDef ret = HAL_I2C_Slave_Transmit(&hi2c1, (uint8_t*)&msg, sizeof(msg), I2C_TIMO);
 
 			if (ret != HAL_OK)
 			{
